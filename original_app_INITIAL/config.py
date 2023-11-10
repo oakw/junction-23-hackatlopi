9b99b2c41df73d85313e54bd2eb738947448ca7f
liskovich <ricardsliskovskis@gmail.com>
2023-11-11 01:26:38 +0200

    chore: clean up env data add question to template

--- original_app_INITIAL/config.py
+++ original_app_INITIAL/config.py
@@ -2,11 +2,13 @@ import os
 import json
 
 class OriginalConfig():
-  def load_agent_config():
+  def load_agent_config(user_question):
     try:
       json_file_path = "agent_config.json"
       with open(json_file_path, 'r') as file:
         data = json.load(file)
+        question_specified = data["goal"][0].replace("<INPUT_QUESTION>", user_question)
+        data["goal"][0] = question_specified
     except:
       # TODO: provide default data
       # TODO: consider proper tool name
@@ -14,18 +16,20 @@ class OriginalConfig():
         "name": "Super AI question answerer",
         "description": "This AI agent is capable of answering any user question, by also supplying resources as a support",
         "goal": [
-          "Find a precise and detailed answer to the question asked by the user (2 paragraphs max.)", 
+          f"Find a precise and detailed answer to the question asked by the user: {user_question}", 
           "Supply sources and argumentation to the answer"
           ],
         "instruction": [
           "Split the question into subquestions", 
-          "Do a Internet search for each subquestion",
+          "Use the tool \"InternetSearch\" to do an Internet search for each subquestion",
           "Summarise the insights from Internet search and add your commentary or explanations",
           "Save your response as a text file"
           ],
         "agent_workflow": "Goal Based Workflow",
         "constraints": [
+          "~4000 word limit for short term memory",
           "Don't ask questions to user to specify unclarities",
+          "Exclusively use the commands listed in double quotes e.g. \"command name\"",
           "Don't make assumptions without any supportive data or evidence"
           ],
         "tools": [
@@ -40,7 +44,18 @@ class OriginalConfig():
   def __init__(self) -> None:
     self.SUPER_AGI_KEY = "SUPER_AGI_KEY"
     self.SUPER_AGI_URL = "SUPER_AGI_URL"
+    
+    self.SERVER_HOST = "SERVER_HOST"
+    self.SERVER_PORT = "SERVER_PORT"
 
+  @classmethod
+  def get_server_data(self):
+    return (
+      os.environ.get(self.SERVER_HOST), 
+      os.environ.get(self.SERVER_PORT)
+    )
+
+  # Super AGI specific
   @classmethod
   def get_super_agi_key(self):
     return os.environ.get(self.SUPER_AGI_KEY)
--- original_app_INITIAL/run.py
+++ original_app_INITIAL/run.py
@@ -1,7 +1,14 @@
 from app.routes import app
+from config import OriginalConfig
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  host, port = OriginalConfig.get_server_data()
+  app.run(
+    debug=True,
+    host=host,
+    port=int(port)
+  )
