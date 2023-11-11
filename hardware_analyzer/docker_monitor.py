4eb5142a4be35f9f8c4acb2e78823f8570071baf
Mārtiņš P <mrcukurs73@gmail.com>
2023-11-11 12:21:56 +0200

    Take CPU power consumption into account

--- hardware_analyzer/docker_monitor.py
+++ hardware_analyzer/docker_monitor.py
@@ -5,16 +5,21 @@ import docker
 import datetime
 import sys
 
+# Intel's RAPL energy API location on Linux
+# Used to measure the CPU's power consumption
+RAPL_ENERGY_API_LOCATION = "/sys/class/powercap/intel-rapl/intel-rapl:0"
 
 # Record of the docker stats object
 class Measurement:
-    def __init__(self, stats_object) -> None:
+    def __init__(self, stats_object: object, cpu_energy: int) -> None:
         self.read_time = datetime.datetime.fromisoformat(stats_object["read"][:-1])
         self.total_cpu_usage = stats_object["cpu_stats"]["cpu_usage"]["total_usage"]
+        self.total_cpu_system_usage = stats_object["cpu_stats"]["system_cpu_usage"]
         self.memory_usage = stats_object["memory_stats"]["usage"]
         self.paged_in_from_disk = stats_object["memory_stats"]["stats"]["total_pgpgin"]
         self.paged_out_to_disk = stats_object["memory_stats"]["stats"]["total_pgpgout"]
         self.networks = dict([(key, stats_object["networks"][key]) for key in stats_object["networks"] if key != "lo"])
+        self.cpu_energy = cpu_energy
 
     def __str__(self) -> str:
         return self.__dict__.__str__()      
@@ -49,9 +54,17 @@ class Summary:
 
         # Quantitative analysis is done by taking the difference between the first and last measurements
         self.cpu_usage_delta = last_measurement.total_cpu_usage - first_measurement.total_cpu_usage
+        self.cpu_system_usage_delta = last_measurement.total_cpu_system_usage - first_measurement.total_cpu_system_usage
         self.memory_usage_delta = last_measurement.memory_usage - first_measurement.memory_usage
         self.paged_in_from_disk_delta = last_measurement.paged_in_from_disk - first_measurement.paged_in_from_disk
         self.paged_out_to_disk_delta = last_measurement.paged_out_to_disk - first_measurement.paged_out_to_disk
+        self.average_cpu_percent = self.cpu_usage_delta / self.cpu_system_usage_delta * 100
+
+        if last_measurement.cpu_energy is not None and first_measurement.cpu_energy is not None:
+            self.cpu_energy_delta = last_measurement.cpu_energy - first_measurement.cpu_energy
+            self.cpu_process_energy_delta = self.cpu_energy_delta * self.average_cpu_percent / 100
+        else:
+            self.cpu_energy_delta = None
 
         self.networks = {}
         for network in last_measurement.networks:
@@ -83,7 +96,7 @@ class DockerMonitor:
     def collect_data(self) -> None:
         for capture in self.stats_stream:
             if self.currently_monitoring:
-                self.results.add_measurement(Measurement(capture))
+                self.results.add_measurement(Measurement(capture, self.get_cpu_power_comsumption()))
             else:
                 break
     
@@ -92,6 +105,13 @@ class DockerMonitor:
     
     def get_summary(self) -> Summary:
         return Summary(self.results)
+    
+    # Get CPU power consumption in microjoules if available
+    def get_cpu_power_comsumption(self) -> int|None:
+        try:
+            return int(open(f"{RAPL_ENERGY_API_LOCATION}/energy_uj", 'r').read())
+        except:
+            return None
 
 
 # Test using the following command:
